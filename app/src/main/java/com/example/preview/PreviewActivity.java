a020c15194cbaaedf1c1edc4488bb1b9c2d3dcc2
Freedom.Ly <freedom.jfl@gmail.com>
2017-01-11 19:18:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/preview/PreviewActivity.java
@@ -0,0 +1,200 @@
+package com.example.preview;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.Matrix;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.hardware.Camera;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.media.session.MediaControllerCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.EOFException;
+import java.util.Arrays;
+
+/**
+ * Created by Freedom.Ly on 2017-1-10 16:29
+ * Email  Freedom.JFL@Live.com
+ */
+public class PreviewActivity extends AppCompatActivity implements SurfaceHolder.Callback, Camera.PreviewCallback {
+
+    Camera mCamera;
+    SurfaceView mSurfaceView;
+    SurfaceHolder mSurfaceHolder;
+
+    private final String TAG = "Preview";
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview);
+
+        initViews();
+    }
+
+    private void initViews() {
+        mSurfaceView = (SurfaceView) findViewById(R.id.preview_sv);
+        mSurfaceHolder = mSurfaceView.getHolder();
+        mSurfaceHolder.addCallback(this);
+        mCamera = getCamera();
+    }
+
+    /**
+     * 初始化相机
+     */
+    private Camera getCamera() {
+        Camera camera;
+        try {
+            camera = Camera.open();
+            camera.setDisplayOrientation(90);
+        } catch (Exception e) {
+            camera = null;
+        }
+        return camera;
+    }
+
+    /**
+     * 释放相机资源
+     */
+    private void releaseCamera() {
+        if (null != mCamera) {
+            mCamera.setPreviewCallback(null);
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    /**
+     * 在SurfaceView中预览相机内容
+     */
+    private void setStartPreview(Camera camera, SurfaceHolder holder) {
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.setDisplayOrientation(90);
+            camera.startPreview();
+            camera.setOneShotPreviewCallback(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 检查是否具有相机功能
+     */
+    private boolean checkCameraHardware(Context context) {
+        return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+
+
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        Log.d(TAG, "surfaceCreated: ");
+        setStartPreview(mCamera, mSurfaceHolder);
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height) {
+        if (null == mSurfaceHolder.getSurface()) {
+            return;
+        }
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        setStartPreview(mCamera, mSurfaceHolder);
+        Log.d(TAG, "surfaceChanged: ");
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        releaseCamera();
+        Log.d(TAG, "surfaceDestroyed: ");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (this.checkCameraHardware(this) && (null == mCamera)) {
+            mCamera = getCamera();
+            if (null != mSurfaceHolder) {
+                setStartPreview(mCamera, mSurfaceHolder);
+            }
+        }
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseCamera();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releaseCamera();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    @Override
+    public void onPreviewFrame(byte[] bytes, Camera camera) {
+        Thread thread = new Thread();
+        try {
+            thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Log.d(TAG, "onPreviewFrame: Get Preview Frame");
+        String data  = Arrays.toString(bytes);
+        Log.i(TAG, "onPreviewFrame: data = " + data);
+        data = null;
+    }
+
+
+//    private void getPreviewImage() {
+//        mCamera.setPreviewCallback(new Camera.PreviewCallback() {
+//            @Override
+//            public void onPreviewFrame(byte[] bytes, Camera camera) {
+//                Camera.Size size = camera.getParameters().getPreviewSize();
+//                try {
+//                    YuvImage image = new YuvImage(bytes, ImageFormat.NV21, size.width, size.height, null);
+//                    if (null != image) {
+//                        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+//                        image.compressToJpeg(new Rect(0, 0, size.width, size.height), 80, stream);
+//
+//                        Bitmap bitmap = BitmapFactory.decodeByteArray(stream.toByteArray(), 0, stream.size());
+//                        // 旋转图片
+//                        Matrix matrix = new Matrix();
+//                        matrix.postRotate(90);
+//
+//                        Bitmap bmp = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+//                        Bitmap bm = Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(), bmp.getHeight(), matrix, true);
+//                    }
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//
+//            }
+//        });
+//    }
+
+
+}
