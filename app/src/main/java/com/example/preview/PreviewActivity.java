437f91c19694a0de2d79e8e3423e203396feda4b
FreedomLy <freedom.jfl@gmail.com>
2017-01-16 10:59:38 +0800

    添加Util.class 将Toast封装的调用封装成一个接口

--- app/src/main/java/com/example/preview/PreviewActivity.java
+++ app/src/main/java/com/example/preview/PreviewActivity.java
@@ -179,7 +179,7 @@ public class PreviewActivity extends AppCompatActivity implements SurfaceHolder.
 //            e.printStackTrace();
 //        }
         Log.d(TAG, "onPreviewFrame: Get Preview Frame");
-        Util.showToast(this, "Get preview frame");
+        Util.showToast(this, "Get preview frames");
 //        String data  = Arrays.toString(bytes);
 //        Log.i(TAG, "onPreviewFrame: data = " + data);
 //        data = null;
--- app/src/main/java/com/example/preview/Util.java
+++ app/src/main/java/com/example/preview/Util.java
@@ -8,16 +8,17 @@ import android.content.Context;
 import android.widget.Toast;
 
 /**
- * 将Toast的调用封装成一个接口
+ * 将Toast的调用封装成一个接口，这样就不会每次调用都生成一个新的Toast对象了
  */
 public class Util {
 
     private static Toast toast;
 
     public static void showToast(Context context, String content){
-        if (toast == null) {
+        if (toast == null) {    // 先判断toast对象是否为空
+            // 如果为空，则调用makeText()
             toast = Toast.makeText(context, content, Toast.LENGTH_SHORT);
-        } else {
+        } else {    // 否则调用setText()来显示新的内容
             toast.setText(content);
         }
         toast.show();
